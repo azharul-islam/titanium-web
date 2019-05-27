9c7d6ffc340835ecc49fe6fead0f06ace1ffe18f
Azharul Islam <azharulislam@Azharuls-MacBook-Pro.local>
2019-05-28 01:36:39 +0300

    v2

--- src/views/Learn-More.vue
+++ src/views/Learn-More.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="about text-container">
-        <h1>LED light bulbs are quickly taking over as the bulb of choice, replacing incandescent and CFL bulbs
-            now that many countries including Bangladesh have instituted mandatory phase-outs.</h1>
+
+        <h1>LED Light Bulbs: Comparison Charts</h1>
+        <p>How to choose the best lighting for your home while spending less and saving energy?</p>
+        <p>LED light bulbs are quickly taking over as the bulb of choice, replacing incandescent and CFL bulbs
+            now that many countries including Bangladesh have instituted mandatory phase-outs.</p>
         <p>CFLs are a temporary solution to energy-efficient lighting, arriving on the market when first generation LED
             bulbs had a narrow and focused light beam and cost too much for many consumers.
             <br>
@@ -121,8 +124,92 @@
                     time.
                 </li>
             </ul>
+        </div>
+        <div class="container container--sm">
+            <h2><span id="equivalentwattages"></span>Equivalent Wattages and Light Output of Incandescent, CFL, and LED Bulbs</h2>
+
+            <table id="tablepress-37" class="tablepress tablepress-id-37">
+                <thead>
+                <tr class="row-1 odd">
+                    <th class="column-1">Light Output</th><th class="column-2">LEDs</th><th class="column-3">CFLs</th><th class="column-4">Incandescents</th>
+                </tr>
+                </thead>
+                <tbody class="row-hover">
+                <tr class="row-2 even">
+                    <td class="column-1">Lumens</td><td class="column-2">Watts</td><td class="column-3">Watts</td><td class="column-4">Watts</td>
+                </tr>
+                <tr class="row-3 odd">
+                    <td class="column-1">450</td><td class="column-2">4-5</td><td class="column-3">8-12</td><td class="column-4">40</td>
+                </tr>
+                <tr class="row-4 even">
+                    <td class="column-1">750-900</td><td class="column-2">6-8</td><td class="column-3">13-18</td><td class="column-4">60</td>
+                </tr>
+                <tr class="row-5 odd">
+                    <td class="column-1">1100-1300</td><td class="column-2">9-13</td><td class="column-3">18-22</td><td class="column-4">75-100</td>
+                </tr>
+                <tr class="row-6 even">
+                    <td class="column-1">1600-1800</td><td class="column-2">16-20</td><td class="column-3">23-30</td><td class="column-4">100</td>
+                </tr>
+                <tr class="row-7 odd">
+                    <td class="column-1">2600-2800</td><td class="column-2">25-28</td><td class="column-3">30-55</td><td class="column-4">150</td>
+                </tr>
+                </tbody>
+            </table>
+            <!-- #tablepress-37 from cache -->
 
         </div>
+        <div class="container container--sm">
+            <h2><span id="comparingfeatures"></span>Comparing the Features of Incandescent, CFL, and LED Light Bulbs</h2>
+
+            <table id="tablepress-31" class="tablepress tablepress-id-31">
+                <thead>
+                <tr class="row-1">
+                    <th class="column-1">&nbsp;</th><th class="column-2">LEDs</th><th class="column-3">CFLs</th><th class="column-4">Incandescents</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="row-2">
+                    <td class="column-1">Frequent On/Off Cycling</td><td class="column-2">no effect<br>
+                </td><td class="column-3">shortens lifespan<br>
+                </td><td class="column-4">yes</td>
+                </tr>
+                <tr class="row-3">
+                    <td class="column-1">Turns on instantly<br>
+                    </td><td class="column-2">yes</td><td class="column-3">slight delay with some CFLs<br>
+                </td><td class="column-4">yes</td>
+                </tr>
+                <tr class="row-4">
+                    <td class="column-1">Durability</td><td class="column-2">durable</td><td class="column-3">fragile</td><td class="column-4">fragile</td>
+                </tr>
+                <tr class="row-5">
+                    <td class="column-1">Heat Emitted<br>
+                    </td><td class="column-2">low (3 btu's/hr)<br>
+                </td><td class="column-3">medium (15 btu's/hr)</td><td class="column-4">high (85 btu's/hr)<br>
+                </td>
+                </tr>
+                <tr class="row-6">
+                    <td class="column-1">Sensitivity to high temperature</td><td class="column-2">some</td><td class="column-3">yes</td><td class="column-4">no</td>
+                </tr>
+                <tr class="row-7">
+                    <td class="column-1">Sensitivity to low temperature<br>
+                    </td><td class="column-2">no</td><td class="column-3">yes</td><td class="column-4">no</td>
+                </tr>
+                <tr class="row-8">
+                    <td class="column-1">Sensitivity to humidity</td><td class="column-2">no</td><td class="column-3">yes</td><td class="column-4">some</td>
+                </tr>
+                <tr class="row-9">
+                    <td class="column-1">Hazardous Materials</td><td class="column-2">none</td><td class="column-3">5 mg mercury/bulb</td><td class="column-4">none</td>
+                </tr>
+                <tr class="row-10">
+                    <td class="column-1">Replacement frequency <br>
+                        (over 25k hours)<br>
+                    </td><td class="column-2">1</td><td class="column-3">2.5</td><td class="column-4">20+</td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
+        <br><br>
     </div>
 </template>
 
